e94415941a58efaf9150f3600226a9de4babd882
evandjefie <evandjefie10@gmail.com>
2023-08-12 21:42:41 +0000

    DatatTable design updated

--- src/components/data_table/DataTable.jsx
+++ src/components/data_table/DataTable.jsx
@@ -41,7 +41,7 @@ function DataTable() {
         ):(
     
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 tab">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead className="text-xs text-white uppercase bg-amber-400">
                 <tr>
                     <th scope="col" className="px-6 py-3">
                         Date
@@ -62,19 +62,19 @@ function DataTable() {
             </thead>
             <tbody>
                 {
-                    (typeof Data.todos == "undefined") ? (
+                    (!data) ? (
                         <p>Chargement...</p>
                     ):(
-                        Data.todos.map((i,d)=>(
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th id={i} scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {d}
+                        data.map((d,i)=>(
+                        <tr className="bg-white border-b hover:bg-gray-50 text-gray-400">
+                            <th id={i} scope="row" className="px-6 py-4 font-medium text-gray-400 whitespace-nowrap ">
+                                {d.dt}
                             </th>
                             <td id={i} className="px-6 py-4">
-                                {d}
+                                {d.title}
                             </td>
                             <td id={i} className="px-6 py-4">
-                                {d}
+                                {d.desc}
                             </td>
                             <td className="px-6 py-4 text-right">
                                 <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
